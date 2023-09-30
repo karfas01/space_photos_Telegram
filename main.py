633a24cc6f4b6fa97afe684859c98ced12fd8b14
karfas <burglary773@gmail.com>
2023-09-30 15:59:53 +0300

    getting data 1
    getting data from the latest SpaceX launch

--- main.py
+++ main.py
@@ -17,5 +17,9 @@ filename = f'{foldername}/dvmn.jpeg'
 url = "https://upload.wikimedia.org/wikipedia/commons/3/3f/HST-SM4.jpeg"
 
 
+url_SpaceX = 'https://api.spacexdata.com/v5/launches/latest'
+response_SpaceX = requests.get(url_SpaceX)
+print(response_SpaceX.json())
+
 
 download_photo(foldername, filename, url)
