7d49150fa8ef0a63775393dd9d3b491884c246dc
karfas <burglary773@gmail.com>
2023-09-30 14:22:00 +0300

    download shoto 1

--- main.py
+++ main.py
@@ -1,5 +1,10 @@
 import requests
 
+filename = 'dvmn.jpeg'
 url = "https://upload.wikimedia.org/wikipedia/commons/3/3f/HST-SM4.jpeg"
+
 response = requests.get(url)
-print(requests)
+response.raise_for_status()
+
+with open(filename, 'wb') as file:
+    file.write(response.content)
