b721ecb7518de2aa862e061fc57c8076c7565dbc
karfas <burglary773@gmail.com>
2023-09-30 15:42:26 +0300

    fixed bugs 1
    removed photo uploading to the function

--- main.py
+++ main.py
@@ -2,19 +2,20 @@ import requests
 import os
 
 
+def download_photo(foldername, filename, url):
+    if not os.path.isdir(foldername):
+        os.mkdir(foldername)
+
+    response = requests.get(url)
+    response.raise_for_status()
+
+    with open(filename, 'wb') as file:
+        file.write(response.content)
 
 foldername = 'images'
 filename = f'{foldername}/dvmn.jpeg'
 url = "https://upload.wikimedia.org/wikipedia/commons/3/3f/HST-SM4.jpeg"
 
-if not os.path.isdir(foldername):
-     os.mkdir(foldername)
-
-response = requests.get(url)
-response.raise_for_status()
-
-with open(filename, 'wb') as file:
-    file.write(response.content)
 
 
-
+download_photo(foldername, filename, url)
