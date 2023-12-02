c93aff2a09cfc5892761c93b3130df54f1c8fd11
karfas <80702492+karfas01@users.noreply.github.com>
2023-12-02 15:35:46 +0300

    publish photo to Telegram channel

--- sending_photos_bot_telegram.py
+++ sending_photos_bot_telegram.py
@@ -4,19 +4,17 @@ from dotenv import load_dotenv
 
 
 
-
-
-
-
-
 def main():
     load_dotenv()
     key_bot = os.getenv("KEY_BOT")
     chanal_name = os.getenv("CHANNEL_NAME")
     bot = telegram.Bot(token=key_bot)
     bot.send_message(text='Hi John!', chat_id=chanal_name)
-    
 
+    folder = os.walk('images')
+    for files in folder:
+        for file_image in files[2]:
+            bot.send_document(chat_id=chanal_name, document=open(f"images/{file_image}", "rb"))
 
 if __name__ == '__main__':
     main()
