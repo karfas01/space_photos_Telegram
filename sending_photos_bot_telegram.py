e8aa14a23ccd4391c2d151d95557e4ee300a0ded
karfas <80702492+karfas01@users.noreply.github.com>
2023-12-02 18:03:53 +0300

    The eternal work of the code

--- sending_photos_bot_telegram.py
+++ sending_photos_bot_telegram.py
@@ -1,20 +1,44 @@
 import os
+import random
 import telegram
+import argparse
+from time import sleep
 from dotenv import load_dotenv
 
 
 
 def main():
+    
+    parser = argparse.ArgumentParser(
+        description='отправка фотографий в telegram канал'
+    )
+    parser.add_argument('--sleep_summaries', type= int, help='время задержни сводки фото в секундах', default=14400)
+    args = parser.parse_args()
+    
     load_dotenv()
     key_bot = os.getenv("KEY_BOT")
     chanal_name = os.getenv("CHANNEL_NAME")
-    bot = telegram.Bot(token=key_bot)
-    bot.send_message(text='Hi John!', chat_id=chanal_name)
 
-    folder = os.walk('images')
-    for files in folder:
-        for file_image in files[2]:
-            bot.send_document(chat_id=chanal_name, document=open(f"images/{file_image}", "rb"))
+    while True:
+        bot = telegram.Bot(token=key_bot)
+        bot.send_message(text='Hi John!', chat_id=chanal_name)
+        folder = os.walk('images')
+        for files in folder:
+            files = files[2]
+            all_files = os.walk("images")
+
+            for array_of_files in all_files:
+                folder, nested_folder, files = array_of_files
+                random.shuffle(files)
+
+                for file_image in files:
+                    bot.send_document(chat_id=chanal_name, document=open(f"images/{file_image}", "rb"))
+                    sleep(4)
+        sleep(args.sleep_summaries)
+    
+
+        
+
 
 if __name__ == '__main__':
     main()
