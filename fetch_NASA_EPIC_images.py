d603e7bb9104f177554ff527542d15876cea271b
karfas <80702492+karfas01@users.noreply.github.com>
2023-12-16 15:04:44 +0300

    Deleted unnecessary variables

--- fetch_NASA_EPIC_images.py
+++ fetch_NASA_EPIC_images.py
@@ -16,15 +16,12 @@ def pictory_epic(key_nasa):
     decrypted_response_epic = response_epic.json()
     for epic_picture_number in range(number_of_repetitions):
         epic_picture = decrypted_response_epic[epic_picture_number]
-        original_name = epic_picture["image"].split("_")
-        original_name_1 = original_name[0]
-        original_name_3 = original_name[2]
-        name_file_img = f"{original_name_1}_1b_{original_name_3}.png"
+        img_name = f"{epic_picture['image']}.png"
         date = epic_picture["date"].split()[0]
         date_year = date.split("-")[0]
         date_month = date.split("-")[1]
         date_day = date.split("-")[2]
-        epic_url_img = f"https://api.nasa.gov/EPIC/archive/natural/{date_year}/{date_month}/{date_day}/png/{name_file_img}"
+        epic_url_img = f"https://api.nasa.gov/EPIC/archive/natural/{date_year}/{date_month}/{date_day}/png/{img_name}"
         filename = f"EPIC_picture_{epic_picture_number}.png"
         download_photo(filename, epic_url_img, payload)
 
